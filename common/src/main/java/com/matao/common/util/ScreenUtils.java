148c5419323216cd360c2b70eb6b143933a82bc7
matao <matao.mardle@gmail.com>
2016-12-11 23:46:53 +0800

    [REV] Add transformation between dp sp and px

--- common/src/main/java/com/matao/common/util/ScreenUtils.java
+++ common/src/main/java/com/matao/common/util/ScreenUtils.java
@@ -29,4 +29,24 @@ public class ScreenUtils {
         DisplayMetrics dm = context.getResources().getDisplayMetrics();
         return dm.heightPixels;
     }
+
+    public static int px2dp(Context context, float px) {
+        float density = context.getResources().getDisplayMetrics().density;
+        return (int) (px / density + 0.5f);
+    }
+
+    public static int dp2px(Context context, float dp) {
+        float density = context.getResources().getDisplayMetrics().density;
+        return (int) (dp * density + 0.5f);
+    }
+
+    public static int px2sp(Context context, float px) {
+        float scaledDensity = context.getResources().getDisplayMetrics().scaledDensity;
+        return (int) (px / scaledDensity + 0.5f);
+    }
+
+    public static int sp2px(Context context, float sp) {
+        float scaledDensity = context.getResources().getDisplayMetrics().scaledDensity;
+        return (int) (sp * scaledDensity + 0.5f);
+    }
 }

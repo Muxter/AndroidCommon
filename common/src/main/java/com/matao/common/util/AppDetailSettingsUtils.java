558f2f6939aebbcf6f8681f13d0ccbb71c17e156
matao <matao.mardle@gmail.com>
2016-12-15 11:47:56 +0800

    [ADD] Add util

---
+++ common/src/main/java/com/matao/common/util/AppDetailSettingsUtils.java
@@ -0,0 +1,24 @@
+package com.matao.common.util;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.provider.Settings;
+
+/**
+ * Created by matao on 2016-12-15 11:44
+ */
+
+public class AppDetailSettingsUtils {
+    private AppDetailSettingsUtils() {
+        throw new AssertionError("AppDetailSettingsUtils cannot be instantiated!");
+    }
+
+    public static void openAppDetailSetting(Context context, String packageName) {
+        Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        Uri uri = Uri.fromParts("package", packageName, null);
+        intent.setData(uri);
+        context.startActivity(intent);
+    }
+}
--- test/src/main/java/com/matao/androidcommon/MainActivity.java
+++ test/src/main/java/com/matao/androidcommon/MainActivity.java
@@ -1,15 +1,13 @@
 package com.matao.androidcommon;
 
-import android.content.Intent;
-import android.net.Uri;
-import android.provider.Settings;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 import com.matao.common.util.AccessibilityUtils;
+import com.matao.common.util.AppDetailSettingsUtils;
 import com.matao.common.util.ScreenUtils;
 import com.matao.common.util.ViewConfigurationUtils;
 
@@ -50,11 +48,7 @@ public class MainActivity extends AppCompatActivity {
 
     private void cleanProcess(View view) {
         for (String mPackage : mPackages) {
-            Intent intent = new Intent();
-            intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
-            Uri uri = Uri.fromParts("package", mPackage, null);
-            intent.setData(uri);
-            startActivity(intent);
+            AppDetailSettingsUtils.openAppDetailSetting(this, mPackage);
         }
     }
 }

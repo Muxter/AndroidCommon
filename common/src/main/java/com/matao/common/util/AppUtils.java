585114355e328e667b2ee4cff7c7f35fb876eaff
matao <matao.mardle@gmail.com>
2016-12-23 00:38:00 +0800

    [ADD] Add AppUtils

---
+++ common/src/main/java/com/matao/common/util/AppUtils.java
@@ -0,0 +1,32 @@
+package com.matao.common.util;
+
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+
+/**
+ * Created by matao on 2016-12-23 00:33
+ */
+
+public class AppUtils {
+
+    public static int getAppVersionCode(Context context) {
+        try {
+            PackageInfo info = context.getPackageManager().getPackageInfo(context.getPackageName(), 0);
+            return info.versionCode;
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        return 1;
+    }
+
+    public static String getAppVersionName(Context context) {
+        try {
+            PackageInfo info = context.getPackageManager().getPackageInfo(context.getPackageName(), 0);
+            return info.versionName;
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
+}
